09a8bd4e1b459300521d23032ab001a99f3b898b
hxtree <matthewheroux@gmail.com>
2020-02-12 10:38:45 -0500

    Add version maturity and release requirements

--- version.php
+++ version.php
@@ -11,3 +11,5 @@
 $plugin->component = 'block_submission_backup';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
 $plugin->version   = 2017011300;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900;        // Requires this Moodle version
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = 'v1,0';
